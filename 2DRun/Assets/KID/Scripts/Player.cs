9bd14c88d892071049e4ead923a3ff74032a8bf4
KID <50666207+KID421@users.noreply.github.com>
2020-06-01 10:39:41 +0800

    場景設計

---
+++ 2DRun/Assets/KID/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("移動速度"), Range(1, 1000)]
+    public float speed = 50;
+    [Header("跳躍高度"), Range(10, 1000)]
+    public float jump = 100;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rig.AddForce(transform.right * speed * Input.GetAxis("Horizontal"));
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) rig.AddForce(transform.up * jump);
+    }
+}
