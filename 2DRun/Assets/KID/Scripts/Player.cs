23ba9afe14b2240758a78e4cd4224f5f4b65d2b5
KID <50666207+KID421@users.noreply.github.com>
2020-06-08 14:42:33 +0800

    玩家與攝影機

--- 2DRun/Assets/KID/Scripts/Player.cs
+++ 2DRun/Assets/KID/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
@@ -6,31 +7,58 @@ public class Player : MonoBehaviour
     public float speed = 50;
     [Header("跳躍高度"), Range(10, 1000)]
     public float jump = 100;
+    [Header("動畫參數：跳躍")]
+    public string paraJump = "跳躍開關";
+    [Header("動畫參數：死亡")]
+    public string paraDead = "死亡觸發";
+    [Header("地板標籤")]
+    public string tagGround = "地板";
+    [Header("中心點位移")]
+    public Vector3 offset;
+    [Header("地板偵測長度")]
+    public float length = 0.1f;
 
     private Rigidbody2D rig;
+    private Animator ani;
+    public bool isGrounded
+    {
+        get
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + offset, transform.up, length);
+            return hit.collider.tag == tagGround;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position + offset, transform.up * length);
+    }
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Jump();
-    }
-
-    private void FixedUpdate()
-    {
         Move();
     }
 
     private void Move()
     {
-        rig.AddForce(transform.right * speed * Input.GetAxis("Horizontal"));
+        ani.SetBool(paraJump, !isGrounded);
+        transform.Translate(transform.right * speed * Time.deltaTime / 50);
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) rig.AddForce(transform.up * jump);
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(transform.up * jump);
+            rig.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
     }
 }
